40de7c866e52e4546d3585c6f377986ae0f03a71
sam smith <singhkd60@gmail.com>
2019-02-06 14:25:38 -0400

    second part done

--- openbook1.py
+++ openbook1.py
@@ -17,3 +17,26 @@ def list_ifname_ip(fout):
 print(list_ifname_ip(fout))
 
 
+def new_configuration(fout):
+	fin=open("new_running_config.cfg")
+	for line in fout:
+		if "ipaddress" in line:
+			line=line.strip()
+			line=line.split()
+			ipadd=line[2]
+			maskadd=line[3]
+			ipadd=line.split()
+			maskadd=line.split()
+			if ipadd[0]=='172' or ipadd[0]=='192':
+				ipadd[0]='10'
+
+			if (maskadd[0]=='255' and maskadd[1]=='255')or maskadd[2]=='255':
+				maskadd[1]=maskadd[2]='0'
+
+			ipadd='ipaddress'+'.'.join(ipadd)+'.'.join(maskadd)
+
+			return fin.write(ipadd)
+
+
+print(new_configuration(fout))
+
